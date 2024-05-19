b5ba14f5f164d560905ebd3af3f52a025412da11
MOX1983 <ussrksenia170506@gmail.com>
2024-05-19 16:13:23 +0300

    "Добавлены начальные данные"

--- 9 sowt/9 sowt/Source.cpp
+++ 9 sowt/9 sowt/Source.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     cout << "Laboratory work #9. GIT\n";
-    cout << "Variant #12. Library Subscription\n";
+    cout << "Variant #12. Программа конференции\n";
     cout << "Author: Ksenia Sharaeva\n";
     return 0;
 }
